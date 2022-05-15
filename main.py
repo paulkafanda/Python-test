d8c316ba270772a6430524aa6d2d746a4648ff39
Paul Kafanda <paulkaf84@gmail.com>
2022-05-15 11:43:13 +0200

    ajout du fichier principal

---
+++ main.py
@@ -0,0 +1,37 @@
+
+def add_new_doctor(nom, prenom, postnom,
+                   tel, matricule, specialisation):
+    pass
+
+
+def add_new_patient(nom, prenom, postnom,
+                    tel, poids, taille, genre,
+                    age, numero_dossier):
+    pass
+
+
+def find_patients(nom):
+    pass
+
+
+def find_patient(numero_dossier):
+    pass
+
+
+def show_patients():
+    pass
+
+
+def show_doctor():
+    pass
+
+
+def save_complaints(complaints):
+    pass
+
+
+def save_doctor_schedule():
+    pass
+
+
+
