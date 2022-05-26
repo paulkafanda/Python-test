529ffdd656e237b9e47974aeff17e5f1148f5663
Paul Kafanda <paulkaf84@gmail.com>
2022-05-26 02:29:04 +0200

    share add_new_doctor from main.py to doctor.py

--- doctor.py
+++ doctor.py
@@ -1,3 +1,4 @@
+import datetime
 
 
 def find_doctor(li_doc: list, matricule: str):
@@ -12,3 +13,33 @@ def find_doctor(li_doc: list, matricule: str):
             return 1
         elif i == len(li_doc) - 1:
             return 0
+
+
+def add_new_doctor(nom, postnom, prenom,
+                   tel, specialisation):
+    """
+
+    :param nom: ex(KAFANDA)
+    :param postnom: ex(NDALA)
+    :param prenom: ex(Paul)
+    :param tel: num de tel
+    :param specialisation: ex(Pediatre)
+    :return: void
+    """
+    li_doctors = []
+    dateact = datetime.datetime.now()
+    annee = str(dateact.year)
+    place = str(len(li_doctor) + 1)
+    matricule = annee[-2:] + (nom[1]).upper() + (postnom[1]).upper() + place.zfill(3)
+
+    li_doctors.append(
+        [
+            nom.upper(),
+            postnom.upper(),
+            prenom.capitalize(),
+            tel,
+            matricule.upper(),
+            specialisation.upper()
+        ]
+    )
+    return li_doctors[0][:]
